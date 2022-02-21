395e6e469319b626bcbe53c2a44f970c6f28ab25
Isabel Maria Guner-Velasco <isabelmariaoboe@gmail.com>
2022-02-21 22:10:02 +0100

    first sturctures and styles

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const url = "https://coronavirus.m.pipedream.net/"
+
+const proxy = "https://noroffcors.herokuapp.com";
+
+const corsFix = url + proxy;
+
+const resultsContainer = document.querySelector(".results");
+
+
+async function callApi(){
+
+}
+
+callApi()
