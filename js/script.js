b4a3921b51a1653b17bb1e1b3f37c702f73859a6
Isabel Maria Guner-Velasco <isabelmariaoboe@gmail.com>
2022-02-21 23:24:21 +0100

    linking files and callApi function

--- js/script.js
+++ js/script.js
@@ -8,6 +8,26 @@ const resultsContainer = document.querySelector(".results");
 
 
 async function callApi(){
+    try {
+        const response = await fetch(corsFix);
+        const json = await response.json();
+        console.log(json);
+
+        for(let i = 0; i<json.length; i++){
+            if(!json[i].Country_Region || !json[i].Confirmed || !json[i].Deaths) {
+                continue;
+            }
+
+            resultsContainer.innerHTML += `<div class="card">
+                                            <h4>${json[i].Country_Region}<h4>
+                                            <h4>${json[i].Confirmed}<h4>
+                                            <h4>${json[i].Deaths}<h4>
+                                            </div>`
+        }    
+    } catch(error){
+        console.log(error);
+        resultsContainer.innerHTML = message("error", error);
+    }
 
 }
 
