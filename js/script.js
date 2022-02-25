475553a1184af57e324c8c55b2726d6f2878a562
Isabel Maria Guner-Velasco <isabelmariaoboe@gmail.com>
2022-02-26 00:36:58 +0100

    form validation

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
-const url = "https://coronavirus.m.pipedream.net/"
+const url = "https://dog-breeds2.p.rapidapi.com/dog_breeds/"
 
-const proxy = "https://noroffcors.herokuapp.com";
+// const proxy = "https://noroffcors.herokuapp.com/";
 
-const corsFix = url + proxy;
+// const corsFix = proxy + url;
 
 const resultsContainer = document.querySelector(".results");
 
 
 async function callApi(){
     try {
-        const response = await fetch(corsFix);
+        const response = await fetch(url);
         const json = await response.json();
         console.log(json);
+    //     // console.log(json.rawData);
+    //     // console.log(json.rawData.length);
+    //     var country = json.rawData;
+    //     for(let i = 0; i<country.length; i++){
+    
+    //         // if(!country[i].Country_Region || !country[i].Confirmed || !country[i].Deaths) {
+    //         //     console.log("continue")
+    //         //     continue;
+    //         // }
 
-        for(let i = 0; i<json.length; i++){
-            if(!json[i].Country_Region || !json[i].Confirmed || !json[i].Deaths) {
-                continue;
-            }
-
-            resultsContainer.innerHTML += `<div class="card">
-                                            <h4>${json[i].Country_Region}<h4>
-                                            <h4>${json[i].Confirmed}<h4>
-                                            <h4>${json[i].Deaths}<h4>
-                                            </div>`
-        }    
+    //         resultsContainer.innerHTML += `<div class="card">
+    //                                         <h4>${country[i].Country_Region}<h4>
+    //                                         <h4>${country[i].Confirmed}<h4>
+    //                                         <h4>${country[i].Deaths}<h4>
+    //                                         </div>`
+    //     }    
     } catch(error){
         console.log(error);
         resultsContainer.innerHTML = message("error", error);
@@ -32,3 +36,5 @@ async function callApi(){
 }
 
 callApi()
+
+
